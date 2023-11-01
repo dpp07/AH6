77abab3b108a78298f98ef263d8b969b4c9bf4dd
Deep Patel <147175602+dpp07@users.noreply.github.com>
2023-11-01 15:55:51 -0400

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.transform.Source;
@@ -14,11 +15,15 @@ class Node<T> {
 
 class Solution {
   public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
+    List <String> myList = new ArrayList<>();
+    while (head != null){
+      myList.add(head.val);
+      head = head.next;
+    }
+    return myList;
   }
 
-  public static void main() {
+  public static void main(String[]args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -29,7 +34,7 @@ class Solution {
 
     // a -> b -> c -> d
 
-    Solution.linkedListValues(a); 
+    System.out.println (Solution.linkedListValues(a));
     // -> [ "a", "b", "c", "d" ]
   }
 }
